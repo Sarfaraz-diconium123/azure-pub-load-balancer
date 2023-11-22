ed4869b8aa0036465ee32a5b41f9f470109ef6b5
Sarfaraz-diconium123 <147616900+Sarfaraz-diconium123@users.noreply.github.com>
2023-11-22 15:15:05 +0100

    Update jumpbox.sh
    update file

--- Azure Load Balancer/jumpbox.sh
+++ Azure Load Balancer/jumpbox.sh
@@ -12,10 +12,11 @@ ssh -o StrictHostKeyChecking=no kodekloud@$ip bash -c  \
 printenv | grep VAR
 echo "Setting up webserver-0$i VM"
 sudo apt install apache2 -y
-sudo mkdir /var/www/
-sudo chmod -R -v 777 /var/www/
-sudo mkdir /var/www/html/
-sudo curl -o /var/www/html/index.html "https://raw.githubusercontent.com/Sarfaraz-diconium123/azure-pub-load-balancer/main/Azure%20Load%20Balancer/sample.html" 
+#sudo mkdir /var/www/
+#sudo chmod -R -v 777 /var/www/
+#sudo mkdir /var/www/html/
+#sudo curl -o /var/www/html/index.html "https://raw.githubusercontent.com/Sarfaraz-diconium123/azure-pub-load-balancer/main/Azure%20Load%20Balancer/sample.html" 
+sudo curl "https://raw.githubusercontent.com/Sarfaraz-diconium123/azure-pub-load-balancer/main/Azure%20Load%20Balancer/sample.html" > /var/www/html/index.html
 sed -i "s/PAGECOLOR/${colors[$i]}/g" /var/www/html/index.html
 exit
 '"
