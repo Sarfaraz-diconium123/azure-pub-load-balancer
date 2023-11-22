a9b60ef00e14ed2cf03819ab7b2bda9bdc85bfce
Sarfaraz-diconium123 <147616900+Sarfaraz-diconium123@users.noreply.github.com>
2023-11-22 10:57:05 +0100

    Update lab-infra.ps1
    change path of jumpbox.sh

--- Azure Load Balancer/lab-infra.ps1
+++ Azure Load Balancer/lab-infra.ps1
@@ -114,7 +114,7 @@ $Params = @{
     Publisher          = 'Microsoft.Azure.Extensions'
     ExtensionType      = 'CustomScript'
     TypeHandlerVersion = '2.1'
-    Settings          = @{fileUris = @('https://raw.githubusercontent.com/rithinskaria/kodekloud-azure/main/Azure%20Load%20Balancer/jumpbox.sh'); commandToExecute = './jumpbox.sh'}
+    Settings          = @{fileUris = @('https://raw.githubusercontent.com/Sarfaraz-diconium123/azure-pub-load-balancer/main/Azure%20Load%20Balancer/sample.html'); commandToExecute = './jumpbox.sh'}
 }
 Set-AzVMExtension @Params
 
